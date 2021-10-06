63416adac73d9e5d185f0a35080ebb616f6449f4
Nkematu Bonaventure <74823094+ChuloCr8v@users.noreply.github.com>
2021-10-06 16:02:23 +0100

    Update signup.js

--- pages/signup.js
+++ pages/signup.js
@@ -2,7 +2,7 @@ import SignupForm from '../comps/Signup'
 
 const  signup = () => {
   return(
-    <div class="flex flex-col h-100 justify-center items-center min-h-screen" >
+    <div className="flex flex-col h-100 justify-center items-center min-h-screen" >
       <SignupForm />
     </div>
     )
